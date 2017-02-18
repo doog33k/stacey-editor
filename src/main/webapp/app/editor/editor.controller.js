bc6ab789690c169f0cba9a8ed015a7d548a84e5b
Ajrarn24 <pascal.verrecchia@gmail.com>
2017-02-18 08:17:34 +0100

    AJRARN: Add completion for SAS characters accepted at the beginning on a statement

--- src/main/webapp/app/editor/editor.controller.js
+++ src/main/webapp/app/editor/editor.controller.js
@@ -17,12 +17,164 @@ var sampleText="proc surveyselect data=EastHigh method=srs n=15 out=sample1;"
         var vm = this;
 
 
+        var langTools = ace.require("ace/ext/language_tools");
         var editor = ace.edit("editor");
         editor.setTheme("ace/theme/textmate");
         editor.getSession().setMode("ace/mode/sas");
         editor.setOptions({
-            enableBasicAutocompletion: true
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true
         });
+        var sasCompleter = {
+            getCompletions: function (editor, session, pos, prefix, callback) {
+                var sasFirstExpList =  ["axis",
+                                        "call",
+                                        "catname",
+                                        "checkpoint execute_always",
+                                        "comment",
+                                        "data",
+                                        "dm",
+                                        "endrsubmit",
+                                        "endsas",
+                                        "filename",
+                                        "footnote",
+                                        "goptions",
+                                        "goption",
+                                        "killtask",
+                                        "legend",
+                                        "libname",
+                                        "listtask",
+                                        "lock",
+                                        "ods",
+                                        "option",
+                                        "options",
+                                        "page",
+                                        "pattern",
+                                        "proc",
+                                        "procedure",
+                                        "quit",
+                                        "rdisplay",
+                                        "resetline",
+                                        "rget",
+                                        "rsubmit",
+                                        "run",
+                                        "run cancel",
+                                        "sasfile",
+                                        "signoff",
+                                        "signon",
+                                        "skip",
+                                        "symbol",
+                                        "sysecho",
+                                        "systask",
+                                        "waitfor",
+                                        "%abort",
+                                        "%armconv",
+                                        "%armend",
+                                        "%armgtid",
+                                        "%arminit",
+                                        "%armjoin",
+                                        "%armproc",
+                                        "%armstop",
+                                        "%armstrt",
+                                        "%armupdt",
+                                        "%copy",
+                                        "%display",
+                                        "%do",
+                                        "%do %until",
+                                        "%do %while",
+                                        "%else",
+                                        "%end",
+                                        "%global",
+                                        "%goto",
+                                        "%if",
+                                        "%include",
+                                        "%input",
+                                        "%let",
+                                        "%list",
+                                        "%local",
+                                        "%macro",
+                                        "%mend",
+                                        "%perfend",
+                                        "%perfinit",
+                                        "%perfstop",
+                                        "%perfstrt",
+                                        "%put",
+                                        "%return",
+                                        "%run",
+                                        "%symdel",
+                                        "%syscall",
+                                        "%sysexec",
+                                        "%syslput",
+                                        "%sysmacdelete",
+                                        "%sysmstoreclear",
+                                        "%sysrput",
+                                        "%until",
+                                        "%while",
+                                        "%window"
+                                        ];
+
+                /*var sasFirstExpListWithPercent =  ["abort",
+                                        "armconv",
+                                        "armend",
+                                        "armgtid",
+                                        "arminit",
+                                        "armjoin",
+                                        "armproc",
+                                        "armstop",
+                                        "armstrt",
+                                        "armupdt",
+                                        "copy",
+                                        "display",
+                                        "do",
+                                        "do %until",
+                                        "do %while",
+                                        "else",
+                                        "end",
+                                        "global",
+                                        "goto",
+                                        "if",
+                                        "include",
+                                        "input",
+                                        "let",
+                                        "list",
+                                        "local",
+                                        "macro",
+                                        "mend",
+                                        "perfend",
+                                        "perfinit",
+                                        "perfstop",
+                                        "perfstrt",
+                                        "put",
+                                        "return",
+                                        "run",
+                                        "symdel",
+                                        "syscall",
+                                        "sysexec",
+                                        "syslput",
+                                        "sysmacdelete",
+                                        "sysmstoreclear",
+                                        "sysrput",
+                                        "until",
+                                        "while",
+                                        "window"
+                                        ];*/
+                var completersList = [];
+                /*if (prefix === '%') {
+                    completersList = sasFirstExpListWithPercent;
+                } else {
+                    completersList = sasFirstExpList;
+                }*/
+                completersList = sasFirstExpList;
+                callback(null, completersList.map(function (sasExp) {
+                    return {
+                        caption: sasExp,
+                        value: sasExp,
+                        meta: "language"
+                    };
+                }));
+            }
+        }
+        langTools.setCompleters([langTools.textCompleter,sasCompleter]);
 
         load();
 
